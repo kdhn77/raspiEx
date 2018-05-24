a0867390c4131a1aeb9d8882e3882abd537ee812
dohwan <kdhn77@gmail.com>
2018-05-24 17:43:32 +0900

    signal,msgq,shrm
    msgq로 서로의pid를 교환 중간에 signal로 주고받음을신호교환함.
    shrm에 데이터를 입력하면 reader는 writer로부터 signal를 받고나면 데이터를 읽는다.

--- msgEx/shmReader.c
+++ msgEx/shmReader.c
@@ -1,19 +1,77 @@
+/* Reader */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#include <signal.h>
+#include <sys/stat.h>
 
 #define SHMSIZE 100
 
+struct my_pid_st{
+	long int my_msg_type;
+	pid_t pid;
+};
+
+	
+void sigHandler(int signo)
+{
+	//SIGUSR1에반응하는핸들러
+}
+
 int main(void)
 {
+	//msg queue관련변수선언
+	int running=1;
+	int msgid;
+	struct my_pid_st mypid;
+	long int msg_to_receive=0;
+	pid_t writer_pid;
+	
+	//sharedmemory관련변수선언
 	void *shared_Mem=(void*)0;
 	int shmid;
 	int *shmaddr;
 	int i;
+	//시그널핸들러등록 공유메모리입력확인을시그널로받음
+	signal(SIGUSR1,sigHandler);
 	
+	msgid=msgget((key_t)1234,0666 |IPC_CREAT);
+	if(msgid==-1)
+	{
+		fprintf(stderr,"msgget failed with error :%d\n",errno);
+		exit(EXIT_FAILURE);
+	}
+	//recieve writer's pid
+	if(msgrcv(msgid,(void*)&mypid,sizeof(mypid),msg_to_receive,0)==-1)
+	{
+		fprintf(stderr,"msgrcv failed with error: %d\n",errno);
+		exit(EXIT_FAILURE);
+	}
+	writer_pid=mypid.pid;
+	printf("received Writer's PID : %d\n",writer_pid);
+	
+	//send mypid
+	mypid.my_msg_type = 2;
+	mypid.pid=getpid();
+	printf("myPID : %d\n",mypid.pid);
+	if (msgsnd(msgid, (void *)&mypid, sizeof(mypid), 0) == -1) 
+	{
+		fprintf(stderr, "msgsnd failed\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("send the myPID to writer\n");
+	
+	//mypid를보내고writer동작하도록깨움.
+	kill(writer_pid,SIGUSR2);
+	
+	pause();
+	//writer가데이터를입력하고시그널을보낸것을받고데이터읽기시작한다.
 	//1. shmget
 	shmid = shmget((key_t)1234,sizeof(int)*SHMSIZE,0666 | IPC_CREAT);
 	if(shmid == -1)
@@ -32,7 +90,6 @@ int main(void)
 	printf("Memory attached at 0x%p\n",shared_Mem);
 	shmaddr=(int*)shared_Mem;
 	
-	sleep(1);
 	//3. memory access
 	for(i=0;i<SHMSIZE;i++)
 	{
@@ -45,11 +102,13 @@ int main(void)
 		fprintf(stderr,"shmdt failed\n");
 		exit(EXIT_FAILURE);
 	}
+	
 	//5. shmctl : IPC_RMID
 	if(shmctl(shmid,IPC_RMID,0)==-1)
 	{
 		fprintf(stderr,"shmctl(IPC_RMID) failed \n");
 		exit(EXIT_FAILURE);
 	}
+	printf("shared memory cleared!\n");
 	exit(EXIT_SUCCESS);
 }
--- msgEx/shmWriter.c
+++ msgEx/shmWriter.c
@@ -1,19 +1,83 @@
+/* writer */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
 
 #define SHMSIZE 100
 
+struct my_pid_st{
+	long int my_msg_type;
+	pid_t pid;
+};
+
+
+void sigHandler(int signo)
+{
+	//SIGUSR2에반응하는핸들러
+}
+
 int main(void)
 {
+	//msg queue관련변수선언
+	int msgid;
+	struct my_pid_st mypid;
+	long int msg_to_receive=0;
+	pid_t reader_pid;
+	//shared memory관련변수선언
 	void *shared_Mem=(void*)0;
 	int shmid;
 	int *shmaddr;
 	int i;
+
 	
+	//시그널핸들러등록 message주고받는거확인을시그널로체크
+	
+	signal(SIGUSR2,sigHandler);
+	
+	msgid=msgget((key_t)1234,0666 |IPC_CREAT);
+	if(msgid==-1)
+	{
+		fprintf(stderr,"msgget failed with error :%d\n",errno);
+		exit(EXIT_FAILURE);
+	}
+	mypid.pid=getpid();
+	printf("myPID : %d\n",mypid.pid);
+	//send mypid
+	mypid.my_msg_type = 1;
+	if (msgsnd(msgid, (void *)&mypid, sizeof(mypid), 0) == -1) 
+	{
+		fprintf(stderr, "msgsnd failed\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("send the myPID to reader\n");
+	
+	//reader가신호를보내줄때까지대기
+	pause();
+	
+		//recieve reader's pid
+	if(msgrcv(msgid,(void*)&mypid,sizeof(mypid),msg_to_receive,0)==-1)
+	{
+		fprintf(stderr,"msgrcv failed with error: %d\n",errno);
+		exit(EXIT_FAILURE);
+	}
+	reader_pid=mypid.pid;
+	printf("received Reader's PID : %d\n",reader_pid);
+	
+	//message queue 해제
+	if(msgctl(msgid,IPC_RMID,0)==-1)
+	{
+		fprintf(stderr,"msgctl(IPC_RMID) failed\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("message queue cleared!\n");
 	//1. shmget
 	shmid = shmget((key_t)1234,sizeof(int)*SHMSIZE,0666 | IPC_CREAT);
 	if(shmid == -1)
@@ -38,19 +102,15 @@ int main(void)
 		*(shmaddr+i)=i+1;
 		printf("shmaddr:%p, data:%d\n",shmaddr+i,*(shmaddr+i));
 	}
-	sleep(4);
-
+	
 	//4. shmdt
 	if(shmdt(shared_Mem)==-1)
 	{
 		fprintf(stderr,"shmdt failed\n");
 		exit(EXIT_FAILURE);
 	}
-	//5. shmctl : IPC_RMID
-	if(shmctl(shmid,IPC_RMID,0)==-1)
-	{
-		fprintf(stderr,"shmctl(IPC_RMID) failed \n");
-		exit(EXIT_FAILURE);
-	}
+	//공유메모리데이터입력후reader가읽도로시그널보냄
+	kill(reader_pid,SIGUSR1);
+	
 	exit(EXIT_SUCCESS);
 }
